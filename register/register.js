96c9fd17f6e7badcc4de0cdfb440dceee4c0b34a
Tomasvg18 <tomasvg18@gmail.com>
2025-06-14 11:39:32 -0600

    Register site and link to main page

---
+++ register/register.js
@@ -0,0 +1,51 @@
+import { participantTemplate } from './template.js';
+
+var count = 1;
+
+function addButton() {
+    count++;
+    participantTemplate(count);
+}
+
+
+function submitForm(event) {
+    event.preventDefault();
+    var fee = totalFees();
+    var name = document.getElementById("adult_name").value;
+ 
+    var message = successTemplate({
+        name: name,
+        count: count,
+        fee: fee
+});
+
+ document.getElementById("regForm").style.display = "none";
+    document.getElementById("summaryLabel").innerHTML = message;
+    document.getElementById("summary").style.display = "block";
+}
+
+export function successTemplate(info) {
+    const s = info.count > 1 ? 's' : '';
+    return `Thank you ${info.name} for registering. You have registered ${info.count} participant${s} and owe $${info.fee} in Fees.`;
+}
+
+
+window.onload = function() {
+    var form = document.querySelector("form");
+    form.onsubmit = submitForm.bind(form);
+    document.getElementById("add").addEventListener("click", addButton);
+}
+
+function totalFees() {
+
+    let feeElements = document.querySelectorAll("[id^=fee]");
+    console.log(feeElements);
+
+    feeElements = [...feeElements];
+
+    let total = feeElements.reduce((sum, input) => {
+        return sum + Number(input.value);
+    }, 0);
+
+    return total;
+}
---
+++ register/template.js
@@ -0,0 +1,41 @@
+export function participantTemplate(count) {
+    var html = `<section class="participant${count}">
+            <p>Participant ${count}</p>
+            <div class="item">
+              <label for="firstname"> First Name<span>*</span></label>
+              <input id="firstname${count}" type="text" name="firstname" value="" required />
+            </div>
+            <div class="item activities">
+              <label for="activity">Activity #<span>*</span></label>
+              <input id="activity${count}" type="text" name="activity" />
+            </div>
+            <div class="item">
+              <label for="fee">Fee ($)<span>*</span></label>
+              <input id="fee${count}" type="number" name="fee" />
+            </div>
+            <div class="item">
+              <label for="date">Desired Date <span>*</span></label>
+              <input id="date${count}" type="date" name="date" />
+            </div>
+            <div class="item">
+              <p>Grade</p>
+              <select>
+                <option selected value="" disabled selected></option>
+                <option value="1">1st</option>
+                <option value="2">2nd</option>
+                <option value="3">3rd</option>
+                <option value="4">4th</option>
+                <option value="5">5th</option>
+                <option value="6">6th</option>
+                <option value="7">7th</option>
+                <option value="8">8th</option>
+                <option value="9">9th</option>
+                <option value="10">10th</option>
+                <option value="11">11th</option>
+                <option value="12">12th</option>
+              </select>
+            </div>
+            </section>`;
+    document.getElementById('add').insertAdjacentHTML('beforebegin', html);
+    
+}
